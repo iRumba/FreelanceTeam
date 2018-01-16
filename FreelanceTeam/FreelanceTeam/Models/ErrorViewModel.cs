c40b3975249376658eac7f79adcb5bb3de3f4011
Анатолий <reloader2003@gmail.com>
2018-01-16 16:58:15 +0700

    Понеслась

---
+++ FreelanceTeam/FreelanceTeam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FreelanceTeam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
